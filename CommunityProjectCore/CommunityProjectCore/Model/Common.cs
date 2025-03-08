3bfd037c31c2473ff3a7c54c375e857040a95570
sudha <sudha@SUDHA>
2025-03-08 16:27:52 +0530

    commit

--- CommunityProjectCore/CommunityProjectCore/Model/Common.cs
+++ CommunityProjectCore/CommunityProjectCore/Model/Common.cs
@@ -4,5 +4,7 @@
     {
      public  static string SuccessMessage = "Record saved succesfully";
      public  static string ErrorMessage = "Record saved failed please try againe !";
+        public static string ErrorMessage1 = "Record deleted saved failed please try againe !";
+
     }
 }
